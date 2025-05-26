3fe608df2b82b27329835e0e6931b10e1175fec2
Israel <100066524+isr431@users.noreply.github.com>
2025-05-26 01:54:29 +0000

    Add more locations

--- main.py
+++ main.py
@@ -15,7 +15,36 @@ subway.set_description("A pitch-black, abandoned subway.")
 dungeon = Cave("Dungeon")
 dungeon.set_description("An eerie complex of prison cells.")
 
+factory = Cave("Factory")
+factory.set_description("A decrepit factory filled with toxic waste.")
+
+armoury = Cave("Armoury")
+armoury.set_description("A dingy cell repurposed as a weapons storage facility.")
+
+tunnel = Cave("Tunnel")
+tunnel.set_description("An narrow tunnel that stretches for a long distance.")
+
+hideout = Cave("Hideout")
+hideout.set_description("A secret room full of amenities and resources.")
+
 cavern.link_cave(mine, "north")
 cavern.link_cave(grotto, "east")
 cavern.link_cave(subway, "south")
 cavern.link_cave(dungeon, "west")
+
+subway.link_cave(cavern, "north")
+subway.link_cave(factory, "south")
+subway.link_cave(hideout, "west")
+
+dungeon.link_cave(tunnel, "north")
+dungeon.link_cave(cavern, "east")
+dungeon.link_cave(hideout, "south")
+dungeon.link_cave(armoury, "west")
+
+tunnel.link_cave(mine, "east")
+tunnel.link_cave(dungeon, "south")
+
+hideout.link_cave(dungeon, "north")
+hideout.link_cave(subway, "east")
+
+armoury.link_cave(dungeon, "east")
