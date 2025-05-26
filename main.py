620972efcb4fdcbaacd8732807be7e582d3cccf3
Israel <100066524+isr431@users.noreply.github.com>
2025-05-26 01:29:22 +0000

    Setup caves

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from cave import Cave
 
 cavern = Cave("Cavern")
-grotto = Cave("Grotto")
+cavern.set_description("A dank and dirty cave ")
+
 dungeon = Cave("Dungeon")
+dungeon.set_description("A large cave with a rack")
+
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient graffiti")
+
+cavern.link_cave(dungeon, "south")
+grotto.link_cave(dungeon, "east")
+dungeon.link_cave(grotto, "west")
